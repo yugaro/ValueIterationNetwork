57cf6b6cc9e7b4d45d64c6e9871279dd88f03db0
yugaro <yugaro.yugaro@gmail.com>
2021-03-22 00:56:31 +0900

    Initial Commit

---
+++ public/train.py
@@ -0,0 +1,136 @@
+import time
+import argparse
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from components.data_generator import GridWorldData
+from utils.utils import print_header
+from utils.utils import print_stats
+from utils.utils import get_stats
+from model.vin import VIN
+
+
+def train_model(model, trainloader, args, criterion, optimizer):
+    print_header()
+
+    # automatically select device to make the code device agnostic
+    print(torch.cuda.is_available())
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model = model.to(device)
+
+    # loop over epochs
+    for epoch in range(args.epochs):
+        avg_error, avg_loss, num_batches = 0.0, 0.0, 0.0
+        start_time = time.time()
+
+        # loop over batches of data
+        for i, data in enumerate(trainloader):
+            # get input data
+            X, S1, S2, labels = [d.to(device) for d in data]
+            if X.size()[0] != args.batch_size:
+                continue  # drop those data if not enough for a batch
+
+            # implement backpropagation and update params
+            optimizer.zero_grad()
+            outputs, predictions = model(X, S1, S2, args.num_vi)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # get stats
+            loss_batch, error_batch = get_stats(loss, predictions, labels)
+            avg_loss += loss_batch
+            avg_error += error_batch
+            num_batches += 1
+        time_duration = time.time() - start_time
+
+        # print epoch logs
+        print_stats(epoch, avg_loss, avg_error, num_batches, time_duration)
+
+    print('\nFinished training. \n')
+
+
+def test_model(model, testloader, args):
+    total, correct = 0.0, 0.0
+    # Automatically select device, device agnostic
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model = model.to(device)
+    for i, data in enumerate(testloader):
+        # Get inputs
+        X, S1, S2, labels = [d.to(device) for d in data]
+        if X.size()[0] != args.batch_size:
+            continue  # Drop those data, if not enough for a batch
+        # Forward pass
+        outputs, predictions = model(X, S1, S2, args.num_vi)
+        # Select actions with max scores(logits)
+        _, predicted = torch.max(predictions, dim=1, keepdim=True)
+        # Unwrap autograd.Variable to Tensor
+        predicted = predicted.data
+        # Compute test accuracy
+        correct += (torch.eq(torch.squeeze(predicted), labels)).sum()
+        total += labels.size()[0]
+    print('Test Accuracy: {:.2f}%'.format(100 * (correct / total)))
+
+
+def set_args():
+    # Parsing training parameters
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--datafile', type=str,
+                        default='data/gridworld_8x8.npz', help='Path to data file')
+    parser.add_argument('--dom_size', type=int,
+                        default=8, help='Size of image')
+    parser.add_argument('--lr', type=float,
+                        default=0.005, help='learning rate, [0.01, 0.005, 0.002, 0.001]')
+    parser.add_argument('--epochs', type=int,
+                        default=30, help='number of epochs to train')
+    parser.add_argument('--num_vi', type=int,
+                        default=10, help='number of Value Iterations')
+    parser.add_argument('--num_input', type=int,
+                        default=2, help='number of channels in input layer')
+    parser.add_argument('--num_hidden', type=int,
+                        default=150, help='number of channels in first hidden layer')
+    parser.add_argument('--num_qlayer', type=int,
+                        default=10, help='number of channels in q layer (~actions) in VI-module')
+    parser.add_argument('--batch_size', type=int,
+                        default=128, help='batch size')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    # set args
+    args = set_args()
+
+    # set path to save trained model
+    save_path = 'data/vin_{0}x{0}.pth'.format(args.dom_size)
+
+    # extract dataset
+    transform = None
+    trainset = GridWorldData(args.datafile, dom_size=args.dom_size,
+                             train=True, transform=transform)
+    testset = GridWorldData(args.datafile, dom_size=args.dom_size,
+                            train=False, transform=transform)
+
+    # make dataloader
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size,
+                                              shuffle=True, num_workers=0)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=args.batch_size,
+                                             shuffle=False, num_workers=0)
+
+    # instantiate VIN model
+    vin = VIN(args)
+
+    # set loss func
+    criterion = nn.CrossEntropyLoss()
+
+    # set optimizer
+    optimizer = optim.RMSprop(vin.parameters(), lr=args.lr, eps=1e-6)
+
+    # train model
+    train_model(model=vin, trainloader=trainloader, args=args,
+                criterion=criterion, optimizer=optimizer)
+
+    # test model
+    test_model(model=vin, testloader=testloader, args=args)
+
+    # save the trained model params
+    torch.save(vin.state_dict(), save_path)
---
+++ src/model/vin.py
@@ -0,0 +1,58 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.parameter import Parameter
+
+
+class VIN(nn.Module):
+    def __init__(self, args):
+        super(VIN, self).__init__()
+        self.args = args
+        self.ly_hidden = nn.Conv2d(in_channels=args.num_input, out_channels=args.num_hidden,
+                                   kernel_size=(3, 3), stride=1, padding=1, bias=True)
+        self.ly_reward = nn.Conv2d(in_channels=args.num_hidden, out_channels=1,
+                                   kernel_size=(1, 1), stride=1, padding=0, bias=False)
+        self.ly_q_value = nn.Conv2d(in_channels=1, out_channels=args.num_qlayer,
+                                    kernel_size=(3, 3), stride=1, padding=1, bias=False)
+        self.fc = nn.Linear(in_features=args.num_qlayer, out_features=8, bias=False)
+        self.weight_v_value = Parameter(torch.zeros(args.num_qlayer, 1, 3, 3), requires_grad=True)
+        self.sm = nn.Softmax(dim=1)
+
+    def forward(self, input_view, state_x, state_y, num_vi):
+        # intermediate output
+        hidden = self.ly_hidden(input_view)
+
+        # get reward
+        reward = self.ly_reward(hidden)
+
+        # get initial q value from reward
+        q_value = self.ly_q_value(reward)
+
+        # get v value
+        v_value, _ = torch.max(q_value, dim=1, keepdim=True)
+
+        def eval_q_value(r, v):
+            return F.conv2d(
+                # concatenate reward with most recent value
+                input=torch.cat([r, v], 1),
+                # convolve reward -> q value weights with reward and v value-> q value weights with v value.
+                weight=torch.cat(
+                    [self.ly_q_value.weight, self.weight_v_value], 1),
+                stride=1,
+                padding=1)
+
+        # Update q and v values
+        for i in range(num_vi - 1):
+            q_value = eval_q_value(reward, v_value)
+            v_value, _ = torch.max(q_value, dim=1, keepdim=True)
+        q_value = eval_q_value(reward, v_value)
+
+        batch_size, l_q, _, _ = q_value.size()
+
+        qvalue_state_xy = q_value[torch.arange(batch_size), :, state_x.long(),
+                                  state_y.long()].view(batch_size, l_q)
+
+        # transform qvalue corresponding to current state into actions
+        logits = self.fc(qvalue_state_xy)
+
+        return logits, self.sm(logits)
